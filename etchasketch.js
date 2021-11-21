f3e62a1e796cd3c7b7bd3853e9f19dcbb4116a77
LemmingsCode <stevecodecamp@gmail.com>
2021-11-21 21:06:42 +1100

    created base files

---
+++ etchasketch.js
@@ -0,0 +1,9 @@
+// create a hover effect so that the grid div changes color when mouse passes over it
+// change color by adding a class to the new div
+// change the div's background color using javascript
+
+
+function changeGridColor (e) {
+}
+
+document.addEventListener('mouseenter', changeGridColor);
