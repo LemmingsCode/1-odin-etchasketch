e210dfb17eaf05b93a7a1f066fb7513306c11b2b
LemmingsCode <stevecodecamp@gmail.com>
2021-11-24 15:33:43 +1100

    cleaned up code to make it easier to read

--- etchasketch.js
+++ etchasketch.js
@@ -3,6 +3,7 @@
 // change the div's background color using javascript
 
 const container = document.getElementById("container");
+initiateGrid()
 
 function makeRows (rows, cols) {
     container.style.setProperty('--grid-rows', rows);
@@ -14,26 +15,31 @@ function makeRows (rows, cols) {
     };
 };
 
-makeRows(16, 16);
-const gridItemsNodeList = container.children; // array like object (nodelist)
-const gridItemsArray = Array.prototype.slice.call(gridItemsNodeList); // convert nodelist to array so we can add event listeners to each item
-
-gridItemsArray.forEach(gridItemsArray => gridItemsArray.addEventListener('mouseover', changeGridColor)); //this only works with arrays
-
 function changeGridColor (e) {
-    console.log(e.target);
-    // gridItemsNodeList.className = "new-grid-color"; How do I make the event listener change the corresponding node color? Can I try if this. operator works?
-    this.style.backgroundColor = "red"; // this binds to the event in this instance, returning all values. this.style looks for the style property within the event. Second question is, is it appropriate to use "this" in this situation or should I find another way to change the background colour?
-
+    this.style.backgroundColor = "red";
 }
+
+function changeGridColorEventListener(array) {
+    array.forEach(array => array.addEventListener('mouseover', changeGridColor)); 
+}
+
+
+function initiateGrid() {
+    makeRows(16, 16);
+    const gridItemsNodeList = container.children; // array like object (nodelist)
+    const gridItemsArray = Array.prototype.slice.call(gridItemsNodeList); // convert nodelist to array so we can add event listeners to each item
+    changeGridColorEventListener(gridItemsArray);
+}
+
 
 function resetGrid () {
     let userInput = window.prompt("Please enter the number of squares to create the grid")
     removeElementsByClass("grid-class");
     makeRows(userInput, userInput);
-    var gridItemsNodeList = container.children; // array like object (nodelist)
-    var gridItemsArray = Array.prototype.slice.call(gridItemsNodeList);
-    gridItemsArray.forEach(gridItemsArray => gridItemsArray.addEventListener('mouseover', changeGridColor));
+    
+    const gridItemsNodeList = container.children; // array like object (nodelist)
+    const gridItemsArray = Array.prototype.slice.call(gridItemsNodeList);
+    changeGridColorEventListener(gridItemsArray);
     gridItemsArray.forEach(gridItemsArray => {
         gridItemsArray.style.backgroundColor = "blue";
     });
