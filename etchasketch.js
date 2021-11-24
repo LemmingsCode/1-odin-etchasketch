1d219a5ffd26e25495cab75bdec3bd238442315d
LemmingsCode <stevecodecamp@gmail.com>
2021-11-24 14:59:41 +1100

    added reset button and let user input size

--- etchasketch.js
+++ etchasketch.js
@@ -16,14 +16,35 @@ function makeRows (rows, cols) {
 
 makeRows(16, 16);
 const gridItemsNodeList = container.children; // array like object (nodelist)
-const gridItemsArray = Array.prototype.slice.call(gridItemsNodeList); ; // convert nodelist to array so we can add event listeners to each item
-console.log(gridItemsArray);
+const gridItemsArray = Array.prototype.slice.call(gridItemsNodeList); // convert nodelist to array so we can add event listeners to each item
 
 gridItemsArray.forEach(gridItemsArray => gridItemsArray.addEventListener('mouseover', changeGridColor)); //this only works with arrays
 
 function changeGridColor (e) {
-    console.log(e);
+    console.log(e.target);
     // gridItemsNodeList.className = "new-grid-color"; How do I make the event listener change the corresponding node color? Can I try if this. operator works?
     this.style.backgroundColor = "red"; // this binds to the event in this instance, returning all values. this.style looks for the style property within the event. Second question is, is it appropriate to use "this" in this situation or should I find another way to change the background colour?
+
 }
+
+function resetGrid () {
+    let userInput = window.prompt("Please enter the number of squares to create the grid")
+    removeElementsByClass("grid-class");
+    makeRows(userInput, userInput);
+    var gridItemsNodeList = container.children; // array like object (nodelist)
+    var gridItemsArray = Array.prototype.slice.call(gridItemsNodeList);
+    gridItemsArray.forEach(gridItemsArray => gridItemsArray.addEventListener('mouseover', changeGridColor));
+    gridItemsArray.forEach(gridItemsArray => {
+        gridItemsArray.style.backgroundColor = "blue";
+    });
+}
+
+function removeElementsByClass(className){
+    const elements = document.getElementsByClassName(className);
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
+
+
 
